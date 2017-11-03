edb759f0490585d964118c8cd2d46969f8110252
Lana Sinapayen <lana@Lanas-MacBook-Pro.local>
2017-11-03 10:45:12 +0900

    radio and 10A motor files

---
+++ MD10A/MD10A.ino
@@ -0,0 +1,39 @@
+
+//define pin name
+#define dir_1 4
+#define pwm_1 3
+//#define switch_1 13
+//#define switch_2 12
+#define dir_2 10
+#define pwm_2 9
+//#define pot A1 //potentiometer
+
+void setup(){
+  //declare pins as INPUT/OUTPUT
+  pinMode(pwm_1,OUTPUT);
+  pinMode(dir_1,OUTPUT);
+  pinMode(pwm_2,OUTPUT);
+  pinMode(dir_2,OUTPUT);
+  //pinMode(pot,INPUT);
+  //pinMode(switch_1, INPUT);
+  //pinMode(switch_2, INPUT);
+  Serial.begin(9600); //I am using Serial Monitor instead of LCD display
+}
+void loop(){
+  int pwm_value;
+
+  delay(7000);
+
+  for (pwm_value=0;pwm_value<=255;pwm_value++){
+    //digitalWrite(dir_1,1); //controls the direction the motor
+    digitalWrite(dir_2,0);
+    analogWrite(pwm_1,pwm_value); //increase the speed of the motor from 0 to 255
+    analogWrite(pwm_2,pwm_value); //decrease the speed of the motor from 255 to 0
+    Serial.println("PWM");
+    Serial.println(pwm_value); //Display the value of PWM
+    delay(100);
+  }
+
+
+  ///while(1) continue; //terminate the program
+}
